b139034dc0d3b346bb00ae653a9b36ee2f4f72fb
Broc Gailit <brocgailit@gmail.com>
2020-04-14 10:38:56 -0700

    Add product recommendations

--- script/application.js
+++ script/application.js
@@ -21,7 +21,7 @@ class NomadicApplication {
     return cart;
   }
 
-  intersect(target, once = false, cb) {
+  intersect(target, once = false, cb, options = { threshold: [0, 0.25, 0.5, 0.75, 1] }) {
     const handleIntersection = ([{isIntersecting, intersectionRatio}]) => {
       target.classList[isIntersecting ? 'add' : 'remove']('is-intersecting');
       if(typeof cb !== 'undefined') {
@@ -31,8 +31,29 @@ class NomadicApplication {
         observer.unobserve(target);
       }
     }
-    const observer = new IntersectionObserver(handleIntersection);
+    const observer = new IntersectionObserver(handleIntersection, options);
     observer.observe(target)
+  }
+
+  async recommendations(product_id, limit, selector, callback) {
+    const responseHTML = await $axios.get('/recommendations/products', {
+      params: {
+        section_id: 'product-recommendations',
+        limit,
+        product_id
+      }
+    }).then(res => res.data);
+
+    if(selector) {
+      const section = document.querySelector(selector);
+      const container = document.createElement("div");
+      container.innerHTML = responseHTML;
+      section.innerHTML = container.querySelector(selector).innerHTML;
+    }
+    if(callback) {
+      callback(responseHTML);
+    }
+    return responseHTML;
   }
 }
 
